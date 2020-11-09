711d8a0dfee5b36e4a1f9af17688570ef65979f5
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-10 03:30:58 +0530

    submit data to backend

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import { API_URL } from "./config";
 
 export default function Form() {
   const [data, setData] = useState();
   const textInputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {}, []);
-
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const textInput = textInputRef.current?.value;
     console.log(textInput);
+    const response = await axios.post(`${API_URL}`, { url: textInput });
+    console.log(response);
   };
 
   return (
