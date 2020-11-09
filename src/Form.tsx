c090e06f1060da30c9556302828ece861b043814
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-10 03:37:04 +0530

    display shortened url after submit

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import { API_URL } from "./config";
 
 export default function Form() {
-  const [data, setData] = useState();
+  const [shortUrl, setShortUrl] = useState<string>();
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const textInput = textInputRef.current?.value;
-    console.log(textInput);
     const response = await axios.post(`${API_URL}`, { url: textInput });
     console.log(response);
+    setShortUrl(`${window.location.origin}/${response.data.short}`);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" ref={textInputRef} />
       <button type="submit">Get Short url</button>
+      {shortUrl}
     </form>
   );
 }
