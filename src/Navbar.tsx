d6c659971e0709a08d117846383f33c5718d840f
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-11 03:46:30 +0530

    update navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,6 +6,8 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -28,18 +30,18 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Shorts
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton
+            aria-label="toggle dark mode"
+            aria-controls="toggle dark mode"
+            aria-haspopup="false"
+            // onClick={handleMenu}
+            color="inherit"
+          >
+            <Brightness4Icon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
