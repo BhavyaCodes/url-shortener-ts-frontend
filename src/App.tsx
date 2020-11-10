38dc2615603483e081291f9a0398a315c59e68be
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-10 21:46:21 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Switch, BrowserRouter, Route } from "react-router-dom";
 import Form from "./Form";
 import Redirect from "./Redirect";
 
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 function App() {
   return (
     <BrowserRouter>
+      <CssBaseline />
       <Switch>
         <Route path="/" exact render={() => <Form />} />
         <Route path="/:shortUrl" exact render={() => <Redirect />} />
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,4 +1,9 @@
 import React, { useState, useRef } from "react";
+
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+import Navbar from "./Navbar";
 import axios from "axios";
 import { API_URL } from "./config";
 
@@ -15,10 +20,18 @@ export default function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="url" ref={textInputRef} />
-      <button type="submit">Get Short url</button>
-      {shortUrl}
-    </form>
+    <div>
+      <Navbar />
+      <Container>
+        <Typography variant="h3" gutterBottom align="center">
+          Shorts - URL Shortening Service
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <input type="url" ref={textInputRef} />
+          <button type="submit">Get Short url</button>
+          {shortUrl}
+        </form>
+      </Container>
+    </div>
   );
 }
