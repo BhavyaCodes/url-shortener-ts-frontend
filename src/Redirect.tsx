86b599a515b40ba01c6f222cfacf8865f392ff54
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-11 18:02:43 +0530

    add 404 page

--- src/Redirect.tsx
+++ src/Redirect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { API_URL } from "./config";
@@ -6,17 +6,21 @@ import { API_URL } from "./config";
 function Redirect() {
   const { shortUrl } = useParams<{ shortUrl: string }>();
   console.log(shortUrl);
+  const [is404, setIs404] = useState(false);
 
   useEffect(() => {
     const getUrl = async () => {
-      const response = await axios.get(`${API_URL}/${shortUrl}`);
-      console.log(response.data);
-      window.location.href = response.data;
+      try {
+        const response = await axios.get(`${API_URL}/${shortUrl}`);
+        window.location.href = response.data;
+      } catch (error) {
+        setIs404(true);
+      }
     };
     getUrl();
   }, [shortUrl]);
 
-  return <div></div>;
+  return <div>{is404 ? <h2>404 - page not found</h2> : null}</div>;
 }
 
 export default Redirect;
